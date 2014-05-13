d9a56485f0316df9f001f1dc9854952a5c7dded9
Conke Hu <conke.hu@maxwit.com>
2014-05-13 04:02:29 -0700

    fix fonts
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- dist/fonts.py
+++ dist/fonts.py
@@ -3,10 +3,11 @@
 import os
 
 def setup(dist, apps):
-	file_svr = 'www.maxwit.org'
-	font_dir = '/usr/share/fonts/truetype/extra'
+	pass
+	#file_svr = 'www.maxwit.org'
+	#font_dir = '/usr/share/fonts/truetype/extra'
 
-	os.system('sudo mkdir -vp ' + font_dir)
-	for font in ['simsun.ttc', 'simhei.ttf']:
-		os.system('wget -P %s http://%s/pub/fonts/%s' % (font_dir, file_svr, font))
-	os.system('fc-cache -f')
+	#os.system('sudo mkdir -vp ' + font_dir)
+	#for font in ['simsun.ttc', 'simhei.ttf']:
+	#	os.system('wget -P %s http://%s/pub/fonts/%s' % (font_dir, file_svr, font))
+	#os.system('fc-cache -f')
