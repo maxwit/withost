5ad20c6cf195ce574ca28ba2572b4a220f78edd1
Conke Hu <conke.hu@maxwit.com>
2014-05-13 02:55:24 -0700

    add fonts.py
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

---
+++ dist/fonts.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import os
+
+def setup(dist, apps):
+	file_svr = 'www.maxwit.org'
+	font_dir = '/usr/share/fonts/truetype/extra'
+
+	os.system('sudo mkdir -vp ' + font_dir)
+	for font in ['simsun.ttc', 'simhei.ttf']:
+		os.system('wget -P %s http://%s/pub/fonts/%s' % (font_dir, file_svr, font))
+	os.system('fc-cache -f')
