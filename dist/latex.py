512efb7c622556d4d9a5c6ab4379adbc62a4d5d2
sandy <sandy@maxwit.com>
2014-05-18 18:40:03 +0800

    add latex.py && update vimrc
    Signed-off-by: sandy <sandy@maxwit.com>

---
+++ dist/latex.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import os, urllib, zipfile, shutil
+
+
+def setup(dist, apps):
+    url = "http://mirrors.ctan.org/macros/latex/contrib"
+    pkgs = "draftwatermark everypage multirow"
+
+    cur_path = os.path.abspath('.')
+    os.chdir('/tmp')
+    for pkg in pkgs.split():
+        pkg_file = pkg + '.zip'
+        urllib.urlretrieve('%s/%s.zip' % (url, pkg), filename = pkg_file)
+        z = zipfile.ZipFile(pkg_file)
+        for name in z.namelist():
+            if name.endswith('/'):
+                os.makedirs(name)
+            else:
+                data = z.read(name)
+                f = open(name, 'w+b')
+                f.write(data)
+                f.close()
+
+        if not os.path.exists('%s/%s.sty' % (pkg, pkg)):
+            os.chdir(pkg)
+            if os.path.exists(pkg + '.ins'):
+	        os.system('xelatex ' + pkg + '.ins')
+            elif os.path.exists(pkg + '.dtx'):
+	        os.system('xelatex ' + pkg + '.dtx')
+            else
+                print 'error: can not generate ' + pkg + '.sty'
+
+        os.chdir('/tmp')
+        shutil.copytree(pkg, '/usr/share/texlive/texmf-dist/tex/latex/' + pkg)
+
+    os.chdir(cur_path)
+    os.system('sudo texhash')
