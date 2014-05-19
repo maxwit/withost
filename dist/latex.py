1af07688bf2cc7e520d21a26f81c1f55601b173c
sandy <sandy@maxwit.com>
2014-05-19 23:09:19 +0800

    fix some bugs
    Signed-off-by: sandy <sandy@maxwit.com>

--- dist/latex.py
+++ dist/latex.py
@@ -1,41 +1,47 @@
 #!/usr/bin/python
 
-import os
-import urllib
-import zipfile
-import shutil
+import os, urllib, zipfile, shutil
 
-def setup(dist, conf, apps):
-	url = 'http://mirrors.ctan.org/macros/latex/contrib'
-	pkgs = ['draftwatermark', 'everypage', 'multirow']
+def setup(conf, apps):
+    url = "http://mirrors.ctan.org/macros/latex/contrib"
+    pkgs = "draftwatermark everypage multirow"
+    dst = "/usr/share/texlive/texmf-dist/tex/latex/"
 
-	cur_path = os.path.abspath('.')
-	os.chdir('/tmp')
+    cur_path = os.path.abspath('.')
+    os.chdir('/tmp')
+    for pkg in pkgs.split():
+        if os.path.exists(dst + pkg):
+            print pkg + ' has already be installed!'
+            continue
 
-	for pkg in pkgs:
-		pkg_file = pkg + '.zip'
-		urllib.urlretrieve('%s/%s.zip' % (url, pkg), filename = pkg_file)
-		z = zipfile.ZipFile(pkg_file)
-		for name in z.namelist():
-			if name.endswith('/'):
-				os.makedirs(name)
-			else:
-				data = z.read(name)
-				f = open(name, 'w+b')
-				f.write(data)
-				f.close()
+        pkg_file = pkg + '.zip'
+        if not os.path.exists(pkg):
+            if not os.path.exists(pkg_file):
+                print 'Download ' + pkg_file + ' start.'
+                urllib.urlretrieve('%s/%s.zip' % (url, pkg), filename = pkg_file)
+                print 'Download ' + pkg_file + ' finish.'
 
-		if not os.path.exists('%s/%s.sty' % (pkg, pkg)):
-			os.chdir(pkg)
-			if os.path.exists(pkg + '.ins'):
-				os.system('xelatex ' + pkg + '.ins')
-			elif os.path.exists(pkg + '.dtx'):
-				os.system('xelatex ' + pkg + '.dtx')
-			else:
-				print 'error: can not generate ' + pkg + '.sty'
+            z = zipfile.ZipFile(pkg_file)
+            for name in z.namelist():
+                if name.endswith('/'):
+                    os.makedirs(name)
+                else:
+                    data = z.read(name)
+                    f = open(name, 'w+b')
+                    f.write(data)
+                    f.close()
 
-		os.chdir('/tmp')
-		shutil.copytree(pkg, '/usr/share/texlive/texmf-dist/tex/latex/' + pkg)
+        if not os.path.exists('%s/%s.sty' % (pkg, pkg)):
+            os.chdir(pkg)
+            if os.path.exists(pkg + '.ins'):
+	        os.system('xelatex ' + pkg + '.ins')
+            elif os.path.exists(pkg + '.dtx'):
+	        os.system('xelatex ' + pkg + '.dtx')
+            else:
+                print 'error: can not generate ' + pkg + '.sty'
 
-	os.chdir(cur_path)
-	os.system('texhash')
+        os.chdir('/tmp')
+        os.system('sudo cp -rvf ' + pkg + ' ' + dst)
+
+    os.chdir(cur_path)
+    os.system('sudo texhash')
