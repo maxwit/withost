b8aff89636cb073f0f53bf68ff45aeeeb71f8ea3
Bad Min <bad.min@maxwit.com>
2014-05-19 08:21:09 -0700

    update
    Signed-off-by: Bad Min <bad.min@maxwit.com>

--- dist/latex.py
+++ dist/latex.py
@@ -3,45 +3,45 @@
 import os, urllib, zipfile, shutil
 
 def setup(conf, apps):
-    url = "http://mirrors.ctan.org/macros/latex/contrib"
-    pkgs = "draftwatermark everypage multirow"
-    dst = "/usr/share/texlive/texmf-dist/tex/latex/"
+	url = 'http://mirrors.ctan.org/macros/latex/contrib'
+	pkgs = ['draftwatermark', 'everypage', 'multirow']
+	dst = '/usr/share/texlive/texmf-dist/tex/latex/'
 
-    cur_path = os.path.abspath('.')
-    os.chdir('/tmp')
-    for pkg in pkgs.split():
-        if os.path.exists(dst + pkg):
-            print pkg + ' has already be installed!'
-            continue
+	cur_path = os.path.abspath('.')
+	os.chdir('/tmp')
+	for pkg in pkgs:
+		if os.path.exists(dst + pkg):
+			print pkg + ' has already be installed!'
+			continue
 
-        pkg_file = pkg + '.zip'
-        if not os.path.exists(pkg):
-            if not os.path.exists(pkg_file):
-                print 'Download ' + pkg_file + ' start.'
-                urllib.urlretrieve('%s/%s.zip' % (url, pkg), filename = pkg_file)
-                print 'Download ' + pkg_file + ' finish.'
+		pkg_file = pkg + '.zip'
+		if not os.path.exists(pkg):
+			if not os.path.exists(pkg_file):
+				print 'Download ' + pkg_file + ' start.'
+				urllib.urlretrieve('%s/%s.zip' % (url, pkg), filename = pkg_file)
+				print 'Download ' + pkg_file + ' finish.'
 
-            z = zipfile.ZipFile(pkg_file)
-            for name in z.namelist():
-                if name.endswith('/'):
-                    os.makedirs(name)
-                else:
-                    data = z.read(name)
-                    f = open(name, 'w+b')
-                    f.write(data)
-                    f.close()
+			z = zipfile.ZipFile(pkg_file)
+			for name in z.namelist():
+				if name.endswith('/'):
+					os.makedirs(name)
+				else:
+					data = z.read(name)
+					f = open(name, 'w+b')
+					f.write(data)
+					f.close()
 
-        if not os.path.exists('%s/%s.sty' % (pkg, pkg)):
-            os.chdir(pkg)
-            if os.path.exists(pkg + '.ins'):
-	        os.system('xelatex ' + pkg + '.ins')
-            elif os.path.exists(pkg + '.dtx'):
-	        os.system('xelatex ' + pkg + '.dtx')
-            else:
-                print 'error: can not generate ' + pkg + '.sty'
+		if not os.path.exists('%s/%s.sty' % (pkg, pkg)):
+			os.chdir(pkg)
+			if os.path.exists(pkg + '.ins'):
+				os.system('xelatex ' + pkg + '.ins')
+			elif os.path.exists(pkg + '.dtx'):
+				os.system('xelatex ' + pkg + '.dtx')
+			else:
+				print 'error: can not generate ' + pkg + '.sty'
 
-        os.chdir('/tmp')
-        os.system('sudo cp -rvf ' + pkg + ' ' + dst)
+		os.chdir('/tmp')
+		os.system('cp -rvf ' + pkg + ' ' + dst)
 
-    os.chdir(cur_path)
-    os.system('sudo texhash')
+	os.chdir(cur_path)
+	os.system('texhash')
