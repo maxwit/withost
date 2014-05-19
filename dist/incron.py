0116f8b39fd86509ebaf3c8c44cbfa3da7e22d66
Build Admin <build.admin@maxwit.com>
2014-05-19 12:20:27 -0700

    update incron.py
    Signed-off-by: Build Admin <build.admin@maxwit.com>

--- dist/incron.py
+++ dist/incron.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 
+import os
+
 def setup(dist, conf, apps):
 	admin = conf['sys.admin']
 
-	allow = open('/etc/incron.allow', 'r+')
-	for line in allow:
-		if line.strip() == admin:
-			allow.close()
-			return
+	if os.path.exists('/etc/incron.allow'):
+		allow = open('/etc/incron.allow', 'r+')
+		for line in allow:
+			if line.strip() == admin:
+				allow.close()
+				return
+	else:
+		allow = open('/etc/incron.allow', 'w+')
 
 	allow.write(admin + '\n')
 	allow.close()
