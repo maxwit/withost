1d63428f708276d61b0ca302baba17f9d4cc0925
Server Admin <server.admin@maxwit.com>
2014-05-20 02:42:56 -0400

    use ArgumentParser instead of OptionParser
    Signed-off-by: Server Admin <server.admin@maxwit.com>

--- user-admin.py
+++ user-admin.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python
 
-# user-admin add -n 'Conke Hu' -m conke.hu@maxwit.com conke
-# user-admin del conke
 # user-admin set -m conke@maxwit.com conke
 
 import os
-from optparse import OptionParser
 from ConfigParser import ConfigParser
+from argparse import ArgumentParser
 
 version = 'v4.2'
 conf = {}
 
-def group_exits(group_name):
+def group_exits(group):
 	fp_group = open('/etc/group', 'r')
 
 	for line in fp_group:
 		group = line.split(':')[0]
-		if group_name == group:
+		if group == group:
 			fp_group.close()
 			return True
 
@@ -24,12 +22,12 @@ def group_exits(group_name):
 
 	return False
 
-def user_exits(user_name):
+def user_exits(user):
 	fp_user = open('/etc/passwd', 'r')
 
 	for line in fp_user:
 		account = line.split(':')
-		if user_name == account[0]:
+		if user == account[0]:
 			fp_user.close()
 			return True
 
@@ -37,32 +35,32 @@ def user_exits(user_name):
 
 	return False
 
-def add_user(user_name, full_name, password, mail, group):
-	print 'Adding user %s ...\n' % user_name
+def add_user(user, full_name, password, mail, group):
+	print 'Adding user %s ...\n' % user
 
 	login = os.getlogin()
-	if user_name == login:
+	if user == login:
 		print "skipping current user!\n"
 		exit()
 
-	if user_exits(user_name):
-		print 'user %s exists!' % user_name
+	if user_exits(user):
+		print 'user %s exists!' % user
 		exit()
 
 	if conf.has_key('sys.group'):
-		user_group = conf['sys.group']
+		group = conf['sys.group']
 
-		if not group_exits(user_group):
-			print 'group %s does not exist!\n' % user_group
+		if not group_exits(group):
+			print 'group %s does not exist!\n' % group
 			exit()
 	else:
 		print "Invalid configuration\n"
 		exit()
 
-	os.system('useradd -g %s -m -s /bin/bash %s' % (user_group,user_name))
-	os.system('echo -e "%s\n%s" | passwd %s' % (password, password, user_name))
+	os.system('useradd -g %s -m -s /bin/bash %s' % (group,user))
+	os.system('echo -e "%s\n%s" | passwd %s' % (password, password, user))
 
-	os.system('usermod -c "%s" %s' % (full_name, user_name))
+	os.system('usermod -c "%s" %s' % (full_name, user))
 
 	# run user-config.py -m mail (optional)
 
@@ -72,20 +70,20 @@ def add_user(user_name, full_name, password, mail, group):
 
 	if apps and 'samba' in apps:
 		print '\nadd samba account\n'
-		os.system('echo -e "%s\n%s" | smbpasswd -s -a %s' % (password, password, user_name))
+		os.system('echo -e "%s\n%s" | smbpasswd -s -a %s' % (password, password, user))
 
-	print 'User %s Added!\n' % user_name
+	print 'User %s Added!\n' % user
 
-def del_user(user_name):
-	print "Delete user %s ..." % user_name
+def del_user(user):
+	print "Delete user %s ..." % user
 
 	login = os.getlogin()
-	if user_name == login:
+	if user == login:
 		print 'cannot delete current user!\n'
 		exit()
 
-	if not user_exits(user_name):
-		print 'user %s does not exist!\n'
+	if not user_exits(user):
+		print 'user %s does not exist!\n' % user
 		exit()
 
 	apps = []
@@ -93,10 +91,10 @@ def del_user(user_name):
 		apps = conf['sys.apps'].split()
 
 	if apps and 'samba' in apps:
-		print 'Delete samba user %s ...' % user_name
-		os.system('smbpasswd -d %s' % user_name)
+		print 'Delete samba user %s ...' % user
+		os.system('smbpasswd -d %s' % user)
 
-	os.system('userdel -r %s' % user_name)
+	os.system('userdel -r %s' % user)
 
 	print
 
@@ -105,21 +103,22 @@ if __name__ == '__main__':
 		print 'pls run as root!'
 		exit()
 
-	opt_parser = OptionParser()
-	opt_parser.add_option('-a', '--add', dest='new_user',
-							help = 'add a new user')
-	opt_parser.add_option('-d', '--del', dest='del_user',
-							help = 'delete user')
-	opt_parser.add_option('-v', '--version', dest='version',
-					  default=False, action='store_true',
-					  help='show WitPowser version')
-	opt_parser.version = 'user-admin ' + version
+	opt_parser = ArgumentParser(description='Add or del the user')
 
-	(opt, args) = opt_parser.parse_args()
+	opt_parser.add_argument('operation', action='store',
+				choices=('add', 'del'), help='what you want to do to user')
+	opt_parser.add_argument('user', action='store',
+				help='user name')
+	opt_parser.add_argument('-f', '--full_name', action='store', dest='full_name',
+							help='full name')
+	opt_parser.add_argument('-m', '--mail',  action='store', dest='email',
+							help='e-mail')
+	opt_parser.add_argument('-p', '--password', action='store', dest='password',
+							help='password')
+	opt_parser.add_argument('-v', '--version', action='version', version='WitPower %s' % version,
+							help='show WitPowser version')
 
-	if opt.version:
-		opt_parser.print_version()
-		exit()
+	args = opt_parser.parse_args()
 
 	cfg_parser = ConfigParser()
 
@@ -132,17 +131,15 @@ if __name__ == '__main__':
 		for (key, value) in cfg_parser.items(sect):
 			conf[sect + '.' + key] = value
 
-	if opt.new_user:
-		user_name = opt.new_user
-		full_name = opt.new_user
-		password = 'wit%s' % user_name
-		mail = user_name + '@maxwit.com'
+	if args.operation == 'add':
+		user = args.user
+		full_name = args.full_name or args.user
+		password = args.password or 'wit%s' % user
+		mail = args.email or user + '@maxwit.com'
 		group = conf['sys.group']
-		add_user(user_name, full_name, password, mail, group)
 
-	elif opt.del_user:
-		user_name = opt.del_user
-		del_user(user_name, conf)
-	else:
-		opt_parser.print_help()
-		exit()
+		add_user(user, full_name, password, mail, group)
+
+	elif args.operation == 'del':
+		user = args.user
+		del_user(user)
