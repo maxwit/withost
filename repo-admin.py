826c335a4bf4116e59d172ea16c70263412a1b25
Conke Hu <conke.hu@maxwit.com>
2014-05-21 08:45:28 -0700

    add repo-admin
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

---
+++ repo-admin.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+import os
+from argparse import ArgumentParser
+
+version = '4.2'
+
+server = '192.168.1.1'
+
+def __repo_add(user, repo):
+	print 'add %s for %s' % (repo, user)
+
+	os.system('sudo -H -i -u git git clone --mirror git://%s/project/%s.git repositories/%s/%s.git'
+			% (server, repo, user, repo))
+
+	home = os.getenv('HOME')
+	conf_fd = open(home + '/gitolite-admin/conf/gitolite.conf', 'a+')
+	conf_fd.write('\n')
+	conf_fd.write('repo %s/%s\n' % (user, repo))
+	conf_fd.write('\tRW+\t\t=\t@%s\n' % user)
+	conf_fd.write('\tR\t\t=\t@all\n')
+	conf_fd.close()
+
+def repo_add(user, repo):
+	print '/home/git/repositories/%s/%s.git' % (user, repo)
+	if os.path.exists('/home/git/repositories/%s/%s.git' % (user, repo)):
+		print 'repo %s exists!' % repo
+		return
+
+	home = os.getenv('HOME')
+	conf_fd = open(home + '/gitolite-admin/conf/gitolite.conf')
+	for line in conf_fd:
+		if line.startswith('@'):
+			user_key = line.split('=')
+			if user_key[0].strip() == '@' + user:
+				__repo_add(user, repo)
+				return
+		elif line.strip() == '':
+			break
+
+	print 'Invalid user ' + user
+
+def repo_del(user, repo):
+	print 'delete %s of %s' % (repo, user)
+
+if __name__ == '__main__':
+	arg_parser = ArgumentParser(description='Add or del the user')
+	arg_parser.add_argument('user', action='store', help='repo maintainer')
+	arg_parser.add_argument('repo', action='store', help='repo name')
+	arg_parser.add_argument('-v', '--version', action='version', version='WitPower %s' % version,
+							help='show WitPowser version')
+
+	arg = arg_parser.parse_args()
+
+	if arg.user == None or arg.repo == None:
+		print arg_parser.print_help()
+		exit()
+
+	repo_add(arg.user, arg.repo)
