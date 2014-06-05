ef3f8e8b89ca9fcd3614cc1e319c30f735744b24
Emily Qin <emily.qin@maxwit.com>
2014-06-05 19:08:34 +0800

    update lamp
    Signed-off-by: Emily Qin <emily.qin@maxwit.com>

--- dist/lamp.py
+++ dist/lamp.py
@@ -3,6 +3,7 @@
 import os
 import tarfile
 import urllib
+from dist import site_admin
 from lib import base
 
 def install_django(dver):
@@ -33,4 +34,12 @@ def install_django(dver):
 def setup(dist, conf, apps):
 	if 'python-django' not in apps:
 		# FIXME
-		install_django('1.6.2')
+		try:
+			import django
+		except ImportError:
+			install_django('1.6.2')
+	site = conf['web.site'].split()
+	sitename = [site[0].replace('.', '_'), site[1].replace('.', '_')]
+	print sitename
+	site_admin.add_site(site[0], sitename[0], os.getlogin())
+	site_admin.add_site(site[1], sitename[1], os.getlogin())
