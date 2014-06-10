2b30892d2ed3d18137eeba11f2fd294fcd559a17
Conke Hu <conke.hu@maxwit.com>
2014-06-10 03:35:37 -0700

    update
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- dist/lamp.py
+++ dist/lamp.py
@@ -1,35 +1,6 @@
 #!/usr/bin/python
 
-import os
-import tarfile
-import urllib
 from dist import site_admin
-from lib import base
-
-def install_django(dver):
-	dmaj = dver[:-2]
-
-	fn = 'Django-%s.tar.gz' % dver
-	filepath = '/tmp/' + fn
-	url = 'https://www.djangoproject.com/m/releases/%s/%s' % (dmaj, fn)
-	if not os.path.exists(filepath):
-		print '%s -> %s:' % (url, filepath)
-		urllib.urlretrieve(url, filepath, base.process)
-		print
-
-	if not os.path.exists(filepath[:-7]):
-		tar = tarfile.open(filepath)
-		print 'extracting '
-		for name in tar.getnames():
-			print '.',
-			tar.extract(name,os.path.dirname(filepath))
-		print
-
-	print 'Installing %s' % fn[:-7]
-	os.system('python %s/setup.py -q install' % filepath[:-7])
-	# FIXME
-	django = '/usr/bin/django-admin'
-	os.rename(django + '.py', django)
 
 def setup(dist, conf, apps):
 	site = conf['web.site'].split()
--- dist/python.py
+++ dist/python.py
@@ -1,10 +1,22 @@
 import os
+import urllib
+from lib import base
+
+def install_pip():
+	dest = '/tmp/get-pip.py'
+	url = 'https://bootstrap.pypa.io/get-pip.py'
+	if not os.path.exists(dest):
+		print '%s -> %s:' % (url, dest)
+		urllib.urlretrieve(url, dest, base.process)
+		print
+
+	print 'Installing pip'
+	return os.system('python ' + dest)
 
 def setup(dist, conf, apps):
 	if 'python-pip' not in apps:
-		print 'TODO: install pip'
-		return
-
-	os.system('pip install -U pip')
+		install_pip()
+	else:
+		os.system('pip install -U pip')
 
 	os.system('pip install django')
