bf4c2356877397fbf062a57681c1a156d5615e2f
Conke Hu <conke.hu@maxwit.com>
2014-06-10 13:35:35 -0700

    update
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- dist/lamp.py
+++ dist/lamp.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
+import os
 from dist import site_admin
 
 def setup(dist, conf, apps):
+	os.system('pip install MySQL-python')
+
 	site = conf['web.site'].split()
 	sitename = [site[0].replace('.', '_'), site[1].replace('.', '_')]
 	print sitename
