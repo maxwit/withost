6cc8c63727e9d2e9f31c95083d042b4f1f07e282
Conke Hu <conke.hu@maxwit.com>
2014-06-11 05:13:25 -0700

    site config
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- dist/python.py
+++ dist/python.py
@@ -2,20 +2,16 @@ import os
 import urllib
 from lib import base
 
-def install_pip():
-	dest = '/tmp/get-pip.py'
-	url = 'https://bootstrap.pypa.io/get-pip.py'
-	if not os.path.exists(dest):
-		print '%s -> %s:' % (url, dest)
-		urllib.urlretrieve(url, dest, base.process)
-		print
-
-	print 'Installing pip'
-	return os.system('python ' + dest)
-
 def setup(dist, conf, apps):
 	if 'python-pip' not in apps:
-		install_pip()
+		dest = '/tmp/get-pip.py'
+		url = 'https://bootstrap.pypa.io/get-pip.py'
+		if not os.path.exists(dest):
+			print '%s -> %s:' % (url, dest)
+			urllib.urlretrieve(url, dest, base.process)
+			print
+		print 'Installing pip'
+		os.system('python ' + dest)
 	else:
 		os.system('pip install -U pip')
 
