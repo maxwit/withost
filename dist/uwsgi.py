c497d6b040c868f72f0130d43055583d14201b6f
Conke Hu <conke.hu@maxwit.com>
2014-06-11 21:48:16 -0700

    nothing
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- dist/uwsgi.py
+++ dist/uwsgi.py
@@ -3,5 +3,12 @@ import os
 def setup(dist, conf, apps):
 	os.system('pip install uwsgi')
 
+	#print 'generating %s (%s) ...' % (site_root, backend or 'UWSGI')
+	#pwd = os.getcwd()
+	#os.chdir(os.path.dirname(site_root))
+	#os.system('django-admin.py startproject ' + main)
+	#os.rename(main, os.path.basename(site_root))
+	#os.chdir(pwd)
+
 def remove(dist, conf, apps):
 	pass
