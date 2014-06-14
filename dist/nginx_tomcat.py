a7286b877a80fe18a98bf38268bee4496d96692d
Sandy Zhou <sandy.zhou@maxwit.com>
2014-06-14 23:11:24 +0800

    add nginx-tomcat support
    Signed-off-by: Sandy Zhou <sandy.zhou@maxwit.com>

--- dist/nginx_tomcat.py
+++
@@ -1,8 +0,0 @@
-import os
-import urllib
-from lib import base
-
-def setup(dist, conf, apps):
-	shutil.copyfile('dist/tomcat.conf', '/etc/nginx/conf.d/')
-	os.system('ln -s /etc/nginx/site-enabled/default /etc/nginx/site-available/tomcat.conf')
-	os.system("service nginx restart")
---
+++ dist/tomcat.py
@@ -0,0 +1,17 @@
+import os
+from lib import base
+from dist import website
+
+def add_site(dist, server_type, server_name, owner):
+	site_root = website.get_site_root(server_type, server_name)
+	pattern = {'__DOCROOT__':site_root, '__SERVERNAME__':server_name}
+	base.render_to_file('/var/lib/tomcat7/Catalina/localhost/%s.xml' % server_name, 'dist/site/tomcat.xml', pattern)
+
+def setup(dist, conf, apps):
+	owner = os.getlogin()
+	for server_name in conf['web.site'].split():
+		add_site(dist, 'tomcat', server_name, os.getlogin())
+		print
+
+def remove(dist, conf, apps):
+	pass
