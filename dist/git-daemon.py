d0b48fb01a9daa70b1495ebf941031df095fa325
Conke Hu <conke.hu@maxwit.com>
2014-06-18 05:08:51 -0700

    fix git-daemon
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- dist/git-daemon.py
+++ dist/git-daemon.py
@@ -7,9 +7,9 @@ import shutil
 def setup(dist, conf, apps):
 	if os.path.exists('/etc/service/git-daemon/'):
 		shutil.copyfile('dist/git/run', '/etc/service/git-daemon/run')
-		os.system("killall git-daemon")
+		#os.system("killall git-daemon")
 	elif os.path.exists('/etc/xinetd.d/'):
 		shutil.copyfile('dist/git/git', '/etc/xinetd.d/git')
-		os.system("systemctl restart xinetd")
+		#os.system("systemctl restart xinetd")
 	else:
 		print 'System not supported!'
