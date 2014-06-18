fd8aa9a2f8f74609046fbf2ae7a145b7e2c84e44
Conke Hu <conke.hu@maxwit.com>
2014-06-18 03:21:18 -0700

    na
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- dist/tomcat.py
+++ dist/tomcat.py
@@ -5,6 +5,7 @@ from dist import website
 def add_site(dist, server_type, server_name, owner):
 	site_root = website.get_site_root(server_type, server_name)
 	pattern = {'__DOCROOT__':site_root, '__SERVERNAME__':server_name}
+	# FIXME: fix path for CentOS 6.x
 	base.render_to_file('/var/lib/tomcat7/Catalina/localhost/%s.xml' % server_name, 'dist/site/tomcat.xml', pattern)
 
 def setup(dist, conf, apps):
