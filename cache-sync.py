3734d498403cff01aa84b549d904d9819a18931a
Conke Hu <conke.hu@maxwit.com>
2014-06-20 00:08:22 -0700

    ifx
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- cache-sync.py
+++ cache-sync.py
@@ -9,17 +9,21 @@ def usage():
 
 def xcopy(dir_src, dir_dst, ext):
 	src_list = os.listdir(dir_src)
-	dst_list = os.listdir(dir_dst)
+	try:
+		dst_list = os.listdir(dir_dst)
+	except:
+		dst_list = []
 
 	for fn in set(src_list) - set(dst_list):
 		src = '/'.join([dir_src, fn])
 		dst = '/'.join([dir_dst, fn])
 
 		if os.path.isdir(src):
-			if not os.path.exists(dst):
-				os.mkdir(dst)
 			xcopy(src, dst, ext)
 		elif fn.endswith(ext):
+			parent = os.path.dirname(dst)
+			if not os.path.exists(parent):
+				os.mkdir(parent)
 			print '%s -> %s' % (src, dst)
 			shutil.copyfile(src, dst)
 
