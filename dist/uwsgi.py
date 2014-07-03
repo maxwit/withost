02a984fd916c90474d46330ef5e2a5b2d03d9d2a
Emily <emily.qin@maxwit.com>
2014-07-03 23:33:46 +0800

    update uwsgi setting
    Signed-off-by: Emily <emily.qin@maxwit.com>

--- dist/uwsgi.py
+++ dist/uwsgi.py
@@ -9,7 +9,12 @@ def setup(dist, conf, apps):
 	base.render_to_file(uwsgi_conf, 'dist/site/uwsgi.ini', pattern)
 
 	# FIXME
-	os.system('uwsgi --ini %s &' % uwsgi_conf)
+	# os.system('uwsgi --ini %s &' % uwsgi_conf)
+	if (os.path.exists('/etc/rc.local')):
+		with open('/etc/rc.local') as f:
+			lines = f.readlines()
+		lines.insert(len(lines) - 1, 'uwsgi --ini %s\n' % uwsgi_conf)
+		open('/etc/rc.local', 'w').writelines(lines)
 
 def remove(dist, conf, apps):
 	pass
