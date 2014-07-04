5ba45710e9c6050b6778373eeb114708bfbd4734
Conke Hu <conke.hu@maxwit.com>
2014-07-04 00:06:05 -0700

    update wsgi.py
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- dist/site/wsgi.py
+++ dist/site/wsgi.py
@@ -1,3 +1,3 @@
 def application(environ, start_response): 
 	start_response('200 OK', [('Content-type', 'text/html')])
-	return [str(environ)]
+	return ['<br/><center>Hello from <b>__SERVERNAME__</b>!</center>']
