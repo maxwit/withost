1b8cab9eda8400dcffd3f8a1a3534ba5338c6052
Conke Hu <conke.hu@maxwit.com>
2014-10-20 17:15:58 +0800

    fix distrib checking

--- dist/distrib.py
+++ dist/distrib.py
@@ -108,32 +108,22 @@ class unix(object):
 def get_distro():
 	os_type = platform.system()
 
-	if os_type == 'Linux':
-		(dist_name, version) = platform.dist()[0:2]
-
-		ver = version.split('.')
-		if len(ver) > 2:
-			version = '.'.join([ver[0], ver[1]])
-
-		if dist_name in ['Debain', 'Ubuntu', 'Mint']:
-			from debian import debian
-			dist = debian((dist_name, version))
-		elif dist_name in ['redhat', 'fedora', 'centos', 'OLinux']:
-			from redhat import redhat
-			dist = redhat((dist_name, version))
-		else:
-			raise Exception(dist_name + ' NOT supported!')
-	elif os_type == 'Darwin':
-		from osx import osx
-
-		dist_name = 'OS X'
-		version = platform.mac_ver()[0]
-		dist = osx((ditrib, version))
-	else:
-		if os_type == 'Windows':
-			version = platform.win32_ver()[0]
-			os_type = os_type + ' ' + version
-
+	if os_type != 'Linux':
 		raise Exception(os_type + ' NOT supported!')
+	
+	(dist_name, version) = platform.dist()[0:2]
+
+	ver = version.split('.')
+	if len(ver) > 2:
+		version = '.'.join([ver[0], ver[1]])
+
+	if dist_name in ['Debain', 'Ubuntu', 'Mint']:
+		from debian import debian
+		dist = debian((dist_name, version))
+	elif dist_name in ['redhat', 'fedora', 'centos', 'OLinux']:
+		from redhat import redhat
+		dist = redhat((dist_name, version))
+	else:
+		raise Exception(dist_name + ' NOT supported!')
 
 	return dist
