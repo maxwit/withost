a57a84302374011691c9274f7a1f008e853aab6a
Ting Yang <ting.yang@maxwit.com>
2015-01-21 22:35:16 +0800

    fix gitolite and mariadb
    Signed-off-by: Ting Yang <ting.yang@maxwit.com>

--- dist/gitolite.py
+++ dist/gitolite.py
@@ -4,19 +4,21 @@ import os
 from dist import gitolite_setup
 
 def install_gitolite():
-	git_path = '/usr/share/gitolite'
+	git_path = '/usr/share/gitolite-master'
 
 	if not os.path.exists(git_path):
 		#git clone git://github.com/sitaramc/gitolite.git $git_path
 		#git clone git://github.com/maxwit/gitolite.git $git_path
-		os.system('git clone git://github.com/sitaramc/gitolite.git ' + git_path)
+		#os.system('git clone git://github.com/sitaramc/gitolite.git ' + git_path)
+		url = "https://github.com/sitaramc/gitolite/archive/master.zip"
+		os.system("wget -P /tmp " + url)
+		#urllib.urlretrieve(url, pkg_file, base.process)
+		os.system("unzip -d %s /tmp/master.zip" % os.path.dirname(git_path))
 
 	os.system(git_path + '/install -ln /usr/bin')
 
 	link = os.readlink('/usr/bin/gitolite')
 	print '/usr/bin/gitolite -> ' + link
-
-
 
 def setup(dist, conf, apps):
 	if 'gitolite' not in apps:
