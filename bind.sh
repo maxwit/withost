9f39740db1e385abae09d7dde781580646170775
Conke Hu <conke.hu@maxwit.com>
2015-09-18 20:06:56 +0800

    add bind.sh as a test case of bind.py
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

---
+++ bind.sh
@@ -0,0 +1,33 @@
+if [ -e /etc/redhat-release ]; then
+	sudo yum install -y bind
+	s=named
+else
+	sudo apt-get install -y bind9
+	s=bind9
+fi
+
+sudo python << EOF
+from app import bind
+bind.setup(9, 18)
+EOF
+
+which systemctl > /dev/null
+if [ $? -eq 0 ]; then
+	sudo systemctl restart $s
+else
+	sudo service $s restart
+fi
+
+code=192.168.3.5
+
+nsupdate << EOF
+server 127.0.0.1
+update add code.maxwit.com 3600 IN A $code
+update add git.maxwit.com 3600 IN A $code
+update add bug.maxwit.com 3600 IN A $code
+update add ci.maxwit.com 3600 IN A $code
+show
+send
+EOF
+
+#grep 127.0.0.1 /etc/resolv.conf || sudo cp resolv.conf /etc/resolv.conf
