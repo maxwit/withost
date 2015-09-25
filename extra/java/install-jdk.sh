7de6da74af6e3778c22b74725dcfb673fffc7ac6
Conke Hu <conke.hu@maxwit.com>
2015-09-25 22:48:31 +0800

    update install-jdk.sh: more intelligent
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- extra/java/install-jdk.sh
+++ extra/java/install-jdk.sh
@@ -1,36 +1,98 @@
 #!/bin/sh
 
-if [ $# = 1 ]; then
-	DIR=~
-	JDK=$1
-elif [ $# = 3 ]; then
-	# FIXME
-	DIR=${2%%/}
-	JDK=$3
-else
-	echo "usage: $0 [-d <InstallPath>] <JDK Package>!"
+function get_version
+{
+	ver=(`basename ${versions[$1]} | awk -F'-' '{print $2}' | awk -F'u' '{print $1}'`)
+	echo $ver
+}
+
+versions=(`ls /mnt/witpub/devel/java/jdk/jdk-*-linux-*.tar.* 2>/dev/null`)
+
+s=${#versions[@]}
+if [ $s -gt 0 ]; then
+	m=0
+	ver_m=`get_version 0`
+
+	for ((i=1; i<$s; i++))
+	do
+		ver_c=`get_version $i`
+
+		if [ $ver_c -gt $ver_m ]; then
+			m=$i
+		fi
+	done
+
+	jdk_tar=${versions[$m]}
+fi
+
+dst_dir=$HOME
+
+while [ $# -gt 0 ]
+do
+	case $1 in
+	-j|--jdk)
+		jdk_tar=$2
+		shift
+		;;
+	-d|--dir)
+		dst_dir=$2
+		shift
+		;;
+	*)
+		echo -e "Invalid option '$opt'\n"
+		exit 1
+	esac
+
+	shift
+done
+
+if [ -z "$jdk_tar" ]; then
+	echo "usage: `basename $0` [-d <dir>] -j <jdk>"
+	exit 1
+elif [ ! -e "$jdk_tar" ]; then
+	echo -e "'$jdk_tar' does NOT exist!\n"
 	exit 1
 fi
 
-echo "Installing JDK ..."
-JAVA_HOME=`tar -tf $JDK | head -n 1`
-JAVA_HOME=${JAVA_HOME%%/*}
-JAVA_HOME=$DIR/$JAVA_HOME
-echo "JAVA_HOME = $JAVA_HOME ..."
+jdk_ver=`tar -tf $jdk_tar | head -n 1`
+JAVA_HOME=$dst_dir/${jdk_ver%%/*}
 
-if [ ! -x $JAVA_HOME/bin/javac ]; then
-	tar xf $JDK -C $DIR || exit 1
-fi
+echo -n "Installing JDK ."
+count=0
+tar xvf $jdk_tar -C $dst_dir | while read line
+do
+	((count++))
+	if [ $((count % 50)) -eq 0 ]; then
+		 echo -n .
+	fi
+done || exit 1
+echo " Done."
 
-sed -i -e '/JAVA_HOME/d' -e '/CLASS_PATH/d' $HOME/.profile
-
-cat >> $HOME/.profile << EOF
+temp=`mktemp`
+cat > $temp << EOF
 export JAVA_HOME=$JAVA_HOME
 export CLASS_PATH=.:\$JAVA_HOME/lib:\$JAVA_HOME/jre/lib
 export PATH=\$JAVA_HOME/bin:\$PATH
 EOF
 
-#source ~/.profile
-#javac -version || exit 1
+while read line
+do
+	eval $line
+done < $temp
 
-echo "JDK successfully installed"
+javac -version
+if [ $? -eq 0 ]; then
+	if [ -e /etc/redhat-release ]; then
+		profile="$HOME/.bash_profile"
+	else
+		profile="$HOME/.profile"
+	fi
+	sed -i '/JAVA_HOME/d' $profile
+	cat $temp >> $profile
+	rm $temp
+	echo "JAVA_HOME = $JAVA_HOME"
+	echo "JDK installed successfully!"
+else
+	rm -rf $JAVA_HOME
+	echo "JDK failed to install!"
+fi
