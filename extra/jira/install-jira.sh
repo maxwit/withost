1a1c477333f917d90ad5e7c718ba83fb31927076
Conke Hu <conke.hu@maxwit.com>
2015-11-29 06:17:19 -0800

    jira
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- extra/jira/install-jira.sh
+++ extra/jira/install-jira.sh
@@ -4,10 +4,24 @@ JIRA_HOME=/var/lib/jira
 mkdir -vp $JIRA_HOME
 export JIRA_HOME=$JIRA_HOME
 
-cd /opt
+INSTALL_PATH=/opt/atlassian-jira-software-7.0.2-standalone
 
-rm -rf atlassian-jira-software-7.0.2-standalone
-tar xf /mnt/witpub/devel/source/atlassian-jira-software-7.0.2-jira-7.0.2.tar.gz || exit 1
+rm -rf $INSTALL_PATH
+cd  `dirname $INSTALL_PATH`
 
-cd atlassian-jira-software-7.0.2-standalone
-./bin/start-jira.sh
+tar xf /mnt/witpub/devel/pm/atlassian-jira-software-7.0.2-jira-7.0.2.tar.gz || exit 1
+
+cat > /etc/init.d/jira << EOF
+case $1 in
+start)
+	$INSTALL_PATH/bin/start-jira.sh
+	;;
+stop)
+	$INSTALL_PATH/bin/stop-jira.sh
+	;;
+esac
+EOF
+
+if [ -e /etc/redhat-release ]; then
+	chkconfig jira on
+fi
