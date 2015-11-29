7f20e85fed235dfed9e028d6045629861f84d96b
conke.hu@maxwit.com <conke.hu@maxwit.com>
2015-11-29 15:04:44 +0800

    jira.sh
    Signed-off-by: conke.hu@maxwit.com <conke.hu@maxwit.com>

--- extra/jira/install-jira.sh
+++ extra/jira/install-jira.sh
@@ -1,3 +1,13 @@
 #! /bin/sh
 
+JIRA_HOME=/var/lib/jira 
+mkdir -vp $JIRA_HOME
+export JIRA_HOME=$JIRA_HOME
 
+cd /opt
+
+rm -rf atlassian-jira-software-7.0.2-standalone
+tar xf /mnt/witpub/devel/source/atlassian-jira-software-7.0.2-jira-7.0.2.tar.gz || exit 1
+
+cd atlassian-jira-software-7.0.2-standalone
+./bin/start-jira.sh
