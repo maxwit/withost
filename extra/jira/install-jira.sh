4869e87c60f5fa2a5e03c9b5484fd4d565ba31c0
Conke Hu <conke.hu@maxwit.com>
2015-11-29 15:27:57 +0800

    fix jira
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- extra/jira/install-jira.sh
+++ extra/jira/install-jira.sh
@@ -9,10 +9,10 @@ INSTALL_PATH=/opt/atlassian-jira-software-7.0.2-standalone
 rm -rf $INSTALL_PATH
 cd  `dirname $INSTALL_PATH`
 
-tar xf /mnt/witpub/devel/pm/atlassian-jira-software-7.0.2-jira-7.0.2.tar.gz || exit 1
+tar xf /mnt/witpub/devel/pm/jira/atlassian-jira-software-7.0.2-jira-7.0.2.tar.gz || exit 1
 
-cat > /etc/init.d/jira << EOF
-case $1 in
+echo cat 'case $1 in' > /etc/init.d/jira
+cat >> /etc/init.d/jira << EOF
 start)
 	$INSTALL_PATH/bin/start-jira.sh
 	;;
@@ -21,6 +21,8 @@ stop)
 	;;
 esac
 EOF
+
+chmod +x /etc/init.d/jira
 
 if [ -e /etc/redhat-release ]; then
 	chkconfig jira on
