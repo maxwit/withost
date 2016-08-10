f44c395d63d974fa04866abe1b65dec286bf1f8e
Conke Hu <conke.hu@maxwit.com>
2016-08-10 17:18:42 +0800

    use china mirror
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- extra/agile/gitlab/install-gitlab.sh
+++ extra/agile/gitlab/install-gitlab.sh
@@ -13,29 +13,79 @@ fi
 #	port=10080
 #fi
 
-if [ -e /etc/redhat-release ]; then
-	installer="yum install -y"
-	$installer curl openssh-server postfix cronie
-	service postfix start
-	chkconfig postfix on
-	lokkit -s http -s ssh
-	curl -sS https://packages.gitlab.com/install/repositories/gitlab/$edition/script.rpm.sh | bash
-elif [ -e /etc/debian_version ]; then
-	installer="apt install -y"
-	$installer curl openssh-server ca-certificates postfix
-	curl -sS https://packages.gitlab.com/install/repositories/gitlab/$edition/script.deb.sh | bash
-else
+#if [ -e /etc/redhat-release ]; then
+#	installer="yum install -y"
+#	which systemctl > /dev/null && {
+#		$installer curl policycoreutils openssh-server openssh-clients
+#		systemctl enable sshd
+#		systemctl start sshd
+#		yum install postfix
+#		systemctl enable postfix
+#		systemctl start postfix
+#		firewall-cmd --permanent --add-service=http
+#		systemctl reload firewalld
+#	} || {
+#		$installer curl openssh-server postfix cronie
+#		service postfix start
+#		chkconfig postfix on
+#		lokkit -s http -s ssh
+#	}
+#	curl -sS https://packages.gitlab.com/install/repositories/gitlab/$edition/script.rpm.sh | bash
+#else
+#	installer="apt-get install -y"
+#	$installer curl openssh-server ca-certificates postfix
+#	curl -sS https://packages.gitlab.com/install/repositories/gitlab/$edition/script.deb.sh | bash
+#fi
+#
+#err=0
+#for ((i=0; i<5; i++))
+#do
+#	$installer $edition
+#	err=$?
+#	[ $err -eq 0 ] && break
+#done
+#[ $err -ne 0 ] && exit 1
+
+if [ $edition != gitlab-ce ]; then
+	echo "only CE supported!"
 	exit 1
 fi
 
-err=0
-for ((i=0; i<5; i++))
-do
-	$installer $edition
-	err=$?
-	[ $err -eq 0 ] && break
-done
-[ $err -ne 0 ] && exit 1
+if [ -e /etc/redhat-release ]; then
+	installer="yum install -y"
+
+	$installer redhat-lsb-core
+	release=`lsb_release -rs`
+	release=(${release//./ })
+
+	if [ ! -e /etc/yum.repos.d/$edition.repo ]; then
+		cat > /etc/yum.repos.d/$edition.repo << _EOF_
+[$edition]
+name=$edition
+baseurl=http://mirrors.tuna.tsinghua.edu.cn/$edition/yum/el${release[0]}
+repo_gpgcheck=0
+gpgcheck=0
+enabled=1
+gpgkey=https://packages.gitlab.com/gpg.key
+_EOF_
+		yum makecache
+	fi
+else
+	#installer="apt-get install -y"
+	installer="apt-get install -y --allow-unauthenticated"
+
+	curl https://packages.gitlab.com/gpg.key 2> /dev/null | sudo apt-key add -
+
+	if [ ! -e /etc/apt/sources.list.d/$edition.list ]; then
+		code=`lsb_release -cs`
+		echo "deb https://mirrors.tuna.tsinghua.edu.cn/$edition/ubuntu $code main" > /etc/apt/sources.list.d/$edition.list
+		apt-get update
+	fi
+fi
+
+if [ ! -d /opt/$edition ]; then
+	$installer $edition || exit 1
+fi
 
 sed -i -e "s#external_url .*#external_url 'http://code.maxwit.com'#" \
 	-e "s/# gitlab_rails\['gitlab_email_from'\] =.*/gitlab_rails['gitlab_email_from'] = 'no-reply@maxwit.com'/" \
@@ -44,7 +94,7 @@ sed -i -e "s#external_url .*#external_url 'http://code.maxwit.com'#" \
 	-e "s/# gitlab_rails\['smtp_address'\] =.*/gitlab_rails['smtp_address'] = 'smtp.exmail.qq.com'/" \
 	-e "s/# gitlab_rails\['smtp_port'\] =.*/gitlab_rails['smtp_port'] = 465 /" \
 	-e "s/# gitlab_rails\['smtp_user_name'\] =.*/gitlab_rails['smtp_user_name'] = 'no-reply@maxwit.com'/" \
-	-e "s/# gitlab_rails\['smtp_password'\] =.*/gitlab_rails['smtp_password'] = 'maxwit2015'/" \
+	-e "s/# gitlab_rails\['smtp_password'\] =.*/gitlab_rails['smtp_password'] = 'MaxWit2016'/" \
 	-e "s/# gitlab_rails\['smtp_domain'\] =.*/gitlab_rails['smtp_domain'] = 'maxwit.com'/" \
 	-e "s/# gitlab_rails\['smtp_authentication'\] =.*/gitlab_rails['smtp_authentication'] = 'login'/" \
 	-e "s/# gitlab_rails\['smtp_enable_starttls_auto'\] =.*/gitlab_rails['smtp_enable_starttls_auto'] = true/" \
