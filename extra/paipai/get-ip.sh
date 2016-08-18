ca576e9995bd5c2b91d1eebfb3623b211a99c514
Conke Hu <conke.hu@maxwit.com>
2016-08-18 22:28:21 +0800

    remove get-ip
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- extra/paipai/get-ip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -e /usr/sbin/aliyun-service ]; then
-	cmd="ifconfig eth0"
-else
-	cmd="ifconfig"
-fi
-
-$cmd | grep 'inet addr' | head -1 | awk '{print $2}' | awk -F':' '{print $2}'
