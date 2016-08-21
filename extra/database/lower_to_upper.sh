a100554e56d741ade4548a5614be14bc9427a8dc
Huai Guan <huai.guan@inspiry.cn>
2016-08-21 15:52:48 +0800

    test
    Signed-off-by: Huai Guan <huai.guan@inspiry.cn>

--- extra/database/lower_to_upper.sh
+++ extra/database/lower_to_upper.sh
@@ -28,7 +28,7 @@ while [ $# -gt 0 ]; do
 done
 
 count=0
-tmp=`mysql -u$user -p$password $database -e "SELECT device_id FROM tb_device;" |  grep "^[a-z0-9]*$" | grep -v "^[0-9]*$"`
+tmp=`mysql -u$user -p$password $database -e "SELECT device_id FROM tb_device;" |  grep "[[:lower:]]"`
 
 for line in $tmp; do
 	mysql -u$user -p$password $database -e "UPDATE tb_device SET device_id=upper('$line') WHERE device_id='$line';"
