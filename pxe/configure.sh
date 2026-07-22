f4a5429343800a1f7457179cf9d551d649a10362
Conke Hu <conke.hu@maxwit.com>
2026-07-22 19:25:33 +0800

    add $2 as network argument
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- pxe/configure.sh
+++ pxe/configure.sh
@@ -1,7 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
 
 netdev=${1:-tap0}
-network="192.168.111.1/24"
+network=$2
+
+if [ -z "$network" ]; then
+    while true
+    do
+        network="192.168.$((RANDOM % 100 + 100))"
+        echo $network
+        ip a | grep $network > /dev/null || break
+    done
+    network="$network.1/24"
+fi
 
 gateway=${network%/*}
 subnet=${gateway%.*}
@@ -35,7 +47,7 @@ enable-tftp
 tftp-root=/srv/tftp
 
 # PXE
-dhcp-boot=u-boot-hi3516ev200-demb.bin,${tftpser}
+dhcp-boot=foo,${tftpser}
 
 log-dhcp
 log-queries
